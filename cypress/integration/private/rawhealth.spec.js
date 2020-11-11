5ae5492e8794c840761f2a0ab965af2d48b06538
Patrizio Bekerle <patrizio@bekerle.com>
2020-11-11 21:34:14 +0100

    Increase timeout

--- cypress/integration/private/rawhealth.spec.js
+++ cypress/integration/private/rawhealth.spec.js
@@ -1,14 +1,14 @@
 describe('Rawhealth Tests', () => {
     it('Test rawhealthandhappiness.com', () => {
         cy
-            .visit(Cypress.env('rawhealth_base_url'))
+            .visit(Cypress.env('rawhealth_base_url'), {timeout: 10000})
             .get('.main-body-wrapper h1')
             .contains('High carbohydrate raw vegan lifestyle and recipes')
     })
 
     it('Test rohundfroh.eu', () => {
         cy
-            .visit(Cypress.env('rohundfroh_base_url'))
+            .visit(Cypress.env('rohundfroh_base_url'), {timeout: 10000})
             .get('.main-body-wrapper h1')
             .contains('Vegane Rohkost - Rezepte (80-10-10) und Blog')
     })
