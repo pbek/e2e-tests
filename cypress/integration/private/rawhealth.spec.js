4be6328c5806adc0da1dc20fa5ce6e76471c43e5
Patrizio Bekerle <patrizio@bekerle.com>
2020-11-11 21:38:47 +0100

    Disable Rawhealth tests because of oom problems

--- cypress/integration/private/rawhealth.spec.js
+++ cypress/integration/private/rawhealth.spec.js
@@ -1,15 +1,15 @@
-describe('Rawhealth Tests', () => {
-    it('Test rawhealthandhappiness.com', () => {
-        cy
-            .visit(Cypress.env('rawhealth_base_url'), {timeout: 10000})
-            .get('.main-body-wrapper h1')
-            .contains('High carbohydrate raw vegan lifestyle and recipes')
-    })
+// describe('Rawhealth Tests', () => {
+//     it('Test rawhealthandhappiness.com', () => {
+//         cy
+//             .visit(Cypress.env('rawhealth_base_url'), {timeout: 10000})
+//             .get('.main-body-wrapper h1')
+//             .contains('High carbohydrate raw vegan lifestyle and recipes')
+//     })
 
-    it('Test rohundfroh.eu', () => {
-        cy
-            .visit(Cypress.env('rohundfroh_base_url'), {timeout: 10000})
-            .get('.main-body-wrapper h1')
-            .contains('Vegane Rohkost - Rezepte (80-10-10) und Blog')
-    })
-})
+//     it('Test rohundfroh.eu', () => {
+//         cy
+//             .visit(Cypress.env('rohundfroh_base_url'), {timeout: 10000})
+//             .get('.main-body-wrapper h1')
+//             .contains('Vegane Rohkost - Rezepte (80-10-10) und Blog')
+//     })
+// })
