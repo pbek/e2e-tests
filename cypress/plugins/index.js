ab9efbb06dfa02d58a32a9ec73cf8bf056c5ff5c
Patrizio Bekerle <patrizio@bekerle.com>
2020-12-03 08:26:15 +0100

    Try to fix out of memory crashes in CI docker container

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,4 +18,14 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+    on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'electron') {
+            // try to fix out of memory crashes in CI docker container
+            launchOptions.args.push('--disable-dev-shm-usage')
+
+            return launchOptions
+        }
+
+        return launchOptions
+    })
 }
