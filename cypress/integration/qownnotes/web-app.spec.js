3ed79ec8f056fa4d71092de5a5cd292ec0a3c7e0
Patrizio Bekerle <patrizio@bekerle.com>
2021-08-02 19:57:10 +0200

    Add QOwnNotes Web App test

---
+++ cypress/integration/qownnotes/web-app.spec.js
@@ -0,0 +1,8 @@
+describe('Web App Tests', () => {
+    it('Test frontpage and overview page', () => {
+        cy
+            .visit(Cypress.env('qownnotes_web_app_url'))
+            .get('body')
+            .contains(' Send images to your QOwnNotes desktop application.')
+    })
+})
