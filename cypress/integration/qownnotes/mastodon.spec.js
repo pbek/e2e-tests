6b9e11a1895a917564654d5fb99135efb58f9cbf
Patrizio Bekerle <patrizio@bekerle.com>
2021-08-20 09:35:56 +0200

    Add Mastodon tests

---
+++ cypress/integration/qownnotes/mastodon.spec.js
@@ -0,0 +1,15 @@
+describe('Web App Tests', () => {
+    it('Test Mastodon QOwnNotes page', () => {
+        cy
+            .visit(Cypress.env('qownnotes_mastodon_base_url') + '/@qownnotes')
+            .get('body')
+            .contains('QOwnNotes')
+    })
+
+    it('Test Mastodon Patrizio page', () => {
+        cy
+            .visit(Cypress.env('qownnotes_mastodon_base_url') + '/@patrizio')
+            .get('body')
+            .contains('Patrizio Bekerle')
+    })
+})
