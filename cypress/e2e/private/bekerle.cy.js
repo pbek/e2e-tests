3cccc9d1c9b7929a828bea16a041d7aeace786fd
Patrizio Bekerle <patrizio@bekerle.com>
2022-09-04 15:36:36 +0200

    Fix bekerle.com end2end tests

--- cypress/e2e/private/bekerle.cy.js
+++ cypress/e2e/private/bekerle.cy.js
@@ -2,7 +2,7 @@ describe('Bekerle.com Tests', () => {
     it('Test frontpage', () => {
         cy
             .visit(Cypress.env('bekerle_base_url'))
-            .get('ul.profile-data')
+            .get('div.header-title')
             .contains('Patrizio Bekerle')
     })
 })
