f668cc7cafe19f784e60274dc4e2896a9d1030c1
Patrizio Bekerle <patrizio@bekerle.com>
2022-09-27 11:07:31 +0200

    Use correct server

--- scripts/server-tests.sh
+++ scripts/server-tests.sh
@@ -2,4 +2,4 @@
 
 # test mail server
 # https://github.com/jetmore/swaks/blob/v20201014.0/doc/base.pod
-swaks --to test@google.com -4 --helo bekerle.com --quit-after RCPT
+swaks --server=mail.bekerle.com --helo bekerle.com --quit-after EHLO
