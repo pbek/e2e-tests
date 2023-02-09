cd4c8587e12e80b09f4101b6153ee1bdf0e41013
Patrizio Bekerle <patrizio@bekerle.com>
2023-02-09 15:54:32 +0100

    Update kimai test to new UI

--- cypress/e2e/private/kimai.cy.js
+++ cypress/e2e/private/kimai.cy.js
@@ -2,7 +2,8 @@ describe('Kimai Tests', () => {
     it('Test login page', () => {
         cy
             .visit(Cypress.env('kimai_base_url'))
-            .get('div.login-logo')
-            .contains('Kimai')
+            .get('div.text-center > h1 > img')
+            .invoke('attr', 'alt')
+            .should('contain', 'Kimai')
     })
 })
