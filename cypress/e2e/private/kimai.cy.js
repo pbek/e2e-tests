3a86aa51b383066066c68bcde0c6415b10ef5271
Patrizio Bekerle <patrizio@bekerle.com>
2023-02-10 19:55:50 +0100

    Revert "Update kimai test to new UI", because kimai:lasted rolled back to the non-dev version
    This reverts commit cd4c8587e12e80b09f4101b6153ee1bdf0e41013.

--- cypress/e2e/private/kimai.cy.js
+++ cypress/e2e/private/kimai.cy.js
@@ -2,8 +2,7 @@ describe('Kimai Tests', () => {
     it('Test login page', () => {
         cy
             .visit(Cypress.env('kimai_base_url'))
-            .get('div.text-center > h1 > img')
-            .invoke('attr', 'alt')
-            .should('contain', 'Kimai')
+            .get('div.login-logo')
+            .contains('Kimai')
     })
 })
