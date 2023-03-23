fc28b938be7121700cf8588c01792188c17478ce
Patrizio Bekerle <patrizio@bekerle.com>
2023-03-23 10:06:46 +0100

    fix: make Kimai tests recognize Kimai 1 and Kimai 2

--- cypress/e2e/private/kimai.cy.js
+++ cypress/e2e/private/kimai.cy.js
@@ -2,7 +2,15 @@ describe('Kimai Tests', () => {
     it('Test login page', () => {
         cy
             .visit(Cypress.env('kimai_base_url'))
-            .get('div.login-logo')
-            .contains('Kimai')
+            .get('body')
+            .then((body) => {
+                if (body.find('div.login-logo').length) {
+                    cy.log('Kimai 1 found')
+                    cy.contains('Kimai')
+                } else if (body.find('div.text-center > h1 > img').length) {
+                    cy.log('Kimai 2 found')
+                    cy.invoke('attr', 'alt').should('contain', 'Kimai')
+                }
+            })
     })
 })
