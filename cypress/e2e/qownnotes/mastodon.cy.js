680b45b73d9b23d1abd065812027182abf598804
Patrizio Bekerle <patrizio@bekerle.com>
2023-10-28 14:16:00 +0200

    mastodon: try to visit base url first

--- cypress/e2e/qownnotes/mastodon.cy.js
+++ cypress/e2e/qownnotes/mastodon.cy.js
@@ -1,6 +1,7 @@
 describe('Web App Tests', () => {
     it('Test Mastodon QOwnNotes page', () => {
         cy
+            .visit(Cypress.env('qownnotes_mastodon_base_url'))
             .visit(Cypress.env('qownnotes_mastodon_base_url') + '/@qownnotes')
             .get('body')
             .contains('QOwnNotes')
@@ -8,6 +9,7 @@ describe('Web App Tests', () => {
 
     it('Test Mastodon Patrizio page', () => {
         cy
+            .visit(Cypress.env('qownnotes_mastodon_base_url'))
             .visit(Cypress.env('qownnotes_mastodon_base_url') + '/@patrizio')
             .get('body')
             .contains('Patrizio Bekerle')
