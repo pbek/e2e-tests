87cf34940daa38202f86eb0ff8ff211f3d937adb
Patrizio Bekerle <patrizio@bekerle.com>
2023-10-29 07:49:13 +0100

    ci: try testing Mastodon JSON API

--- cypress/e2e/qownnotes/mastodon.cy.js
+++ cypress/e2e/qownnotes/mastodon.cy.js
@@ -1,19 +1,36 @@
-describe('Web App Tests', () => {
-    it('Test Mastodon QOwnNotes page', () => {
-        cy
-            .visit(Cypress.env('qownnotes_mastodon_base_url'))
-            .visit(Cypress.env('qownnotes_mastodon_base_url') + '/@qownnotes')
-            .wait(3000)
-            .get('body')
-            .contains('QOwnNotes')
+describe('Mastodon Tests', () => {
+    it('Test Mastodon JSON API', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://social.qownnotes.org/api/v1/instance',
+        }).as('response');
+
+        cy.get('@response').should((response) => {
+            expect(response.status).to.eq(200); // Check the HTTP status code
+            expect(response.body).to.have.property('uri'); // Check if uri property exists
+            expect(response.body.uri).to.eq('social.qownnotes.org'); // Check the property value
+            expect(response.body).to.have.property('title'); // Check if title property exists
+            expect(response.body.title).to.eq('QOwnNotes Mastodon'); // Check the property value
+        });
     })
 
-    it('Test Mastodon Patrizio page', () => {
-        cy
-            .visit(Cypress.env('qownnotes_mastodon_base_url'))
-            .visit(Cypress.env('qownnotes_mastodon_base_url') + '/@patrizio')
-            .wait(3000)
-            .get('body')
-            .contains('Patrizio Bekerle')
-    })
+    /**
+     * We have issues testing those on GitHub Actions
+     */
+
+    // it('Test Mastodon QOwnNotes page', () => {
+    //     cy
+    //         .visit(Cypress.env('qownnotes_mastodon_base_url'))
+    //         .visit(Cypress.env('qownnotes_mastodon_base_url') + '/@qownnotes')
+    //         .get('body')
+    //         .contains('QOwnNotes')
+    // })
+    //
+    // it('Test Mastodon Patrizio page', () => {
+    //     cy
+    //         .visit(Cypress.env('qownnotes_mastodon_base_url'))
+    //         .visit(Cypress.env('qownnotes_mastodon_base_url') + '/@patrizio')
+    //         .get('body')
+    //         .contains('Patrizio Bekerle')
+    // })
 })
