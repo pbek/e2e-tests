02ae8818f60928af1494a4f43a2c68e0b9b80db1
Patrizio Bekerle <patrizio@bekerle.com>
2024-09-16 07:25:41 +0200

    qownnotes/api#11 ci: fix qownnotes-api tests for API Platform 3
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- cypress/e2e/qownnotes/api-frontend.cy.js
+++ cypress/e2e/qownnotes/api-frontend.cy.js
@@ -1,25 +1,25 @@
-describe('API Frontend Tests', () => {
+    describe('API Frontend Tests', () => {
     it('Test fetching of latest release', () => {
         cy.visit(Cypress.env('qownnotes_api_base_url'), {headers: {accept: 'text/html'}})
 
         // Click on "LatestRelease collection"
         cy
-            .get('#operations-LatestRelease-getLatestReleaseItem > div.opblock-summary.opblock-summary-get span.opblock-summary-path > a > span')
+            .get('#operations-LatestRelease-api_latest_releases_get_collection > div:nth-child(1) > button:nth-child(3)')
             .click()
 
         // Click on "Try it out"
         cy
-            .get('#operations-LatestRelease-getLatestReleaseItem button.try-out__btn')
+            .get('  button.btn.try-out__btn')
             .click()
 
         // Click on "Execute"
         cy
-            .get('#operations-LatestRelease-getLatestReleaseItem button.execute')
+            .get('.execute-wrapper button.execute.opblock-control__btn')
             .click({force: true})   // even click the button if it is covered
 
         // Look for the download link
         cy
-            .get('#operations-LatestRelease-getLatestReleaseItem')
+            .get('pre.microlight:nth-child(3)')
             .contains('https://github.com/pbek/QOwnNotes/releases/download', {timeout: 10000})
     })
 
