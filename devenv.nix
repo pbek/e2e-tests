735ba6c24a9a8f2db12c80ffbbb367e54a70007d
Patrizio Bekerle <patrizio@bekerle.com>
2025-12-30 14:24:24 +0100

    devenv: use shared config
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- devenv.nix
+++ devenv.nix
@@ -1,30 +1,18 @@
-{
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 
 {
   # https://devenv.sh/packages/
   packages = with pkgs; [
-    just
-    nodejs
     cypress
-
-    # For treefmt
-    treefmt
-    nodePackages.prettier
-    shfmt
-    nixfmt-rfc-style
-    statix
-    taplo
   ];
 
   enterShell = ''
     echo "🛠️ e2e-tests dev shell"
   '';
 
-  # https://devenv.sh/git-hooks/
-  git-hooks.hooks.treefmt.enable = true;
+  git-hooks.hooks = {
+    eslint.enable = false;
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
--- justfile
+++ justfile
@@ -1,21 +1,11 @@
 # Use `just <recipe>` to run a recipe
 # https://just.systems/man/en/
 
+import ".shared/common.just"
+
 # By default, run the `--list` command
 default:
     @just --list
 
-# Aliases
-
-alias fmt := format
-
 cypress-open:
     cypress
-
-cypress-open-steam:
-    NIXPKGS_ALLOW_UNFREE=1 nix-shell -p steam-run --run "steam-run npx cypress open"
-
-# Format all files
-[group('linter')]
-format args='':
-    treefmt {{ args }}
