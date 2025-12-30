d138ff15314805b536063b395b137966e8e8ab80
Patrizio Bekerle <patrizio@bekerle.com>
2025-12-30 14:24:42 +0100

    webpage: fix tests for new vuepress 2 webpage
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- cypress/e2e/qownnotes/webpage.cy.js
+++ cypress/e2e/qownnotes/webpage.cy.js
@@ -1,10 +1,13 @@
 describe("Webpage Tests", () => {
   it("Test frontpage and overview page", () => {
     cy.visit(Cypress.env("qownnotes_webpage_base_url"))
-      .get("header .description")
-      .contains("Free open source plain-text file markdown note-taking")
+      .get("main p")
+      .contains(
+        "Free open source plain-text file markdown note-taking with Nextcloud / ownCloud integration",
+      )
 
-      .get("header a.action-button")
+      .get("main a")
+      .contains("Quick Start")
       .click()
 
       .get("main h2#features")
